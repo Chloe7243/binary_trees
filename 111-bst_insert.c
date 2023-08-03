94a47dacc0d65f050938a3a45dacfb4f32927ea6
Stephanie <Stephaniechloe21@gmail.com>
2023-08-03 13:42:57 -0700

    task insert

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,5 +1,21 @@
 #include "binary_trees.h"
 
+bst_t *helper(bst_t **tree, bst_t *parent, int value)
+{
+	bst_t *node;
+
+	if (!*tree)
+	{
+		node = binary_tree_node(parent, value);
+		return (*tree = node);
+	}
+	else if ((*tree)->n > value)
+		return (helper(&(*tree)->left, *tree, value));
+	else if ((*tree)->n < value)
+		return (helper(&(*tree)->right, *tree, value));
+	return (NULL);
+}
+
 /**
  * bst_insert - inserts a value in a Binary Search Tree
  * @tree: a double pointer to the root node of the BST to insert the value
@@ -9,5 +25,8 @@
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-	if (v
+	if (!tree)
+		return (NULL);
+
+	return (helper(tree, NULL, value));
 }
