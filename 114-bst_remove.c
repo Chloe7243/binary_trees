91045fe2f3f1fd660e706b8e4262d45f375267f2
Stephanie <Stephaniechloe21@gmail.com>
2023-08-03 18:08:33 -0700

    114

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,6 +1,27 @@
 #include "binary_trees.h"
 
 /**
+ * bst_search - searches for a value in a Binary Search Tree
+ * @tree: a pointer to the root node of the BST to search
+ * @value: the value to search in the tree
+ * Return: A pointer to the node containing an int equal to `value`
+ *         NULL if tree is NULL
+ *         NULL if no match is found
+ */
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (!tree)
+		return (NULL);
+	if (tree->n == value)
+		return ((bst_t *)(tree));
+	else if (tree->n < value)
+		return (bst_search(tree->right, value));
+	else if (tree->n > value)
+		return (bst_search(tree->left, value));
+	return (NULL);
+}
+
+/**
  * left_most - finds the smallest node from a Binary Search Tree
  * @node: a pointer to the root node of the tree
  * Return: a pointer to the left_most node
@@ -24,31 +45,24 @@ bst_t *left_most(bst_t *node)
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
-	bst_t *repl;
+	bst_t *node, *repl;
 
 	if (!root)
 		return (NULL);
-
-	if (value < root->n)
-		root->left = bst_remove(root->left, value);
-	else if (value > root->n)
-		root->right = bst_remove(root->right, value);
+	node = bst_search(root, value);
+	if (node->right)
+	{
+		repl = left_most(node->right);
+		node->n = repl->n;
+	}
 	else
 	{
-		if (root->right)
-		{
-			repl = left_most(root->right);
-			root->n = repl->n;
-		}
+		repl = node->left;
+		if (node->parent)
+			node->parent->right = repl;
 		else
-		{
-			repl = root->left;
-			if (root->parent)
-				root->parent->right = repl;
-			else
-				root = repl;
-			repl->parent = root->parent;
-		}
+			root = repl;
+		repl->parent = node->parent;
 	}
 	return (root);
 }
