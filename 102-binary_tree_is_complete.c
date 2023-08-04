5b22f03207fd892f63f37eaeb224fee7e11d9cb4
Stephanie <Stephaniechloe21@gmail.com>
2023-08-03 17:53:55 -0700

    ugh

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,25 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_complete - checks if a binary tree is complete
- * @tree: a pointer to the root node of the tree to check
- *
- * Return: 1 if the tree is complete
- *         0 if the tree is not complete
- *         0 if tree is NULL
- */
-int binary_tree_is_complete(const binary_tree_t *tree)
-{
-	size_t size;
-
-	if (!tree)
-		return (0);
-	size = binary_tree_size(tree);
-
-	return (btic_helper(tree, 0, size));
-}
-
-/**
  * btic_helper - checks if a binary tree is complete
  * @tree: a pointer to the root node of the tree to check
  * @index: node index to check
@@ -39,6 +20,25 @@ int btic_helper(const binary_tree_t *tree, size_t index, size_t size)
 
 	return (btic_helper(tree->left, 2 * index + 1, size) &&
 		btic_helper(tree->right, 2 * index + 2, size));
+}
+
+/**
+ * binary_tree_is_complete - checks if a binary tree is complete
+ * @tree: a pointer to the root node of the tree to check
+ *
+ * Return: 1 if the tree is complete
+ *         0 if the tree is not complete
+ *         0 if tree is NULL
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	size_t size;
+
+	if (!tree)
+		return (0);
+	size = binary_tree_size(tree);
+
+	return (btic_helper(tree, 0, size));
 }
 
 /**
